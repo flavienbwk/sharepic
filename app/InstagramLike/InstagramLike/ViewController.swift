ad39d4ccfa1566bf2935dccf5c42edbb4d5ff1ca
Jonathan Meslien <meslie_j@etna-alternance.net>
2019-03-20 10:28:25 +0100

    init-project

---
+++ app/InstagramLike/InstagramLike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramLike
+//
+//  Created by Jonathan Meslien on 20/03/2019.
+//  Copyright © 2019 Jonathan Meslien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ app/InstagramLike/InstagramLikeTests/InstagramLikeTests.swift
@@ -0,0 +1,34 @@
+//
+//  InstagramLikeTests.swift
+//  InstagramLikeTests
+//
+//  Created by Jonathan Meslien on 20/03/2019.
+//  Copyright © 2019 Jonathan Meslien. All rights reserved.
+//
+
+import XCTest
+@testable import InstagramLike
+
+class InstagramLikeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
