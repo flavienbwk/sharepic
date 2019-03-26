8df2f9f564087e46e4f2a4519ceab4638d7795ab
jonathan <meslien>
2019-03-26 16:30:23 +0100

    update file login controller

--- app/InstagramLike/InstagramLike/Controllers/LoginController.swift
+++ app/InstagramLike/InstagramLike/Controllers/LoginController.swift
@@ -12,6 +12,10 @@ class LoginController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
 }
+
+
