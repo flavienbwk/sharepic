ae89d27ce1d48eccc01593bc5fe569239d66297a
jonathan <meslien>
2019-03-26 16:28:44 +0100

    new file LoginController

---
+++ app/InstagramLike/InstagramLike/Controllers/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  InstagramLike
+//
+//  Created by Jonathan Meslien on 26/03/2019.
+//  Copyright © 2019 Jonathan Meslien. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
